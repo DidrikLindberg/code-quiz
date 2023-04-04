f82756a8eee625bbefb9609a67f032af329bceb6
GitDidrik <lindberg.didrik@gmail.com>
2023-04-03 21:04:20 -0700

    final styling and makeup

--- java.js
+++ java.js
@@ -2,6 +2,8 @@
 
 // event listener that initiates a countdown function when a button "start Quiz" is clicked
 // add listener to listen for event on button element "start Quiz"
+var btnStyle = "cursor:pointer;margin-top:20px;padding:10px 20px;border-radius:4px;border:none;text-align:center;text-decoration:none;display:inline-block;font-size:16px;font-weight:bold;vertical-align:middle;"
+
 var startButton = document.getElementById("startQuiz");
 var timerEl = document.getElementById("timer");
 var questionEl = document.getElementById("question");
@@ -13,19 +15,40 @@ var IncorrectEl = document.getElementById("incorrect");
 //grab the question elements frm the dom
 var questions = [
 {
-    question:"what is JavaScript?",
-    choices: ["A programming language", "A type of coffee", "A type of computer", "A type of dog"],
-    answer: "A programming language"
+    question:"Which of the following type of variable takes precedence over other if names are same?",
+    choices: ["global variable", "local variable", "Both of the above.", "None of the above."],
+    answer: "local variable"
 },
-{   question: "what is a variable?",
+{   question: "A variable is _________",
     choices: ["A container for a value", "A type of coffee", "A type of computer", "A type of dog"],
     answer: "A container for a value"
 },
 {  
-    question:"what is a function?",
-    choices: ["A container for a value", "A type of coffee", "A type of computer", "A type of dog"],
-    answer: "A container for a value"
+    question:"Which of the following function of Array object extracts a section of an array and returns a new array?",
+    choices: ["reverse()", "shift()", "slice()", "some()"],
+    answer: "slice()"
 },
+{  
+    question:"What is a JavaScript framework?",
+    choices: ["A set of libraries and tools for developing web applications", "A programming language used for database management", "A type of computer", "A type of dog"],
+    answer: "A set of libraries and tools for developing web applications"
+},
+{  
+    question:" CSS stands for _________",
+    choices: ["Cascadian Styling Sheet", "Communal Styling Sheet", "Computer Social Skills", "Custom Server Sheet"],
+    answer: "Cascadian Styling Sheet"
+},
+{  
+    question:"A function is _________",
+    choices: ["A mathematical equation", "A vegetable", "A block of code designed to perform a particular task", "A block of code written in plain english"],
+    answer: "A block of code designed to perform a particular task"
+},
+{  
+    question:"HTML stands for _________",
+    choices: ["Home Tool Markup Language", "Hyper Text Markup Language", "Hyperdiaper and Text Markup Language", "hoochie mama text markup language"],
+    answer: "Hyper Text Markup Language"
+},
+
 
 
 ];
@@ -61,12 +84,6 @@ function startCountdown() {
         clearInterval(timeInterval);
         timerEl.textContent = "Time is up!";
 
-        // end of quiz, display score
-        leaderboards();
-
-        // hide the question and choices
-        choicesEl.style.display = "none";
-        questionEl.textContent = "Quiz over. You scored " + score + " out of " + questions.length + ".";
         
         
         }
@@ -170,7 +187,6 @@ function checkAnswer(event) {
             scoreEl.textContent = rank + ".     " + score.initials + " - " + score.score;
             highScoreEl.appendChild(scoreEl);
         }
-        var btnStyle = "cursor:pointer;margin-top:20px;padding:10px 20px;border-radius:4px;border:none;text-align:center;text-decoration:none;display:inline-block;font-size:16px;font-weight:bold;vertical-align:middle;";
   
         var goBackButton = document.createElement("btn");
         goBackButton.textContent = "Go Back";
